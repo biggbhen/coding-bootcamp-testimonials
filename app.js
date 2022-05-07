2ab94b642df8bdeb812eadf2ca197e9db047cfb2
biggbhen <96218403+biggbhen@users.noreply.github.com>
2022-05-07 23:42:39 +0100

    almost done with the functions

--- app.js
+++ app.js
@@ -1,7 +1,62 @@
 const UIimage = document.querySelectorAll('.carousel-image');
+const UItestimony = document.querySelectorAll('.testimony');
 const nextBtn = document.querySelector('.next-btn');
+const previousBtn = document.querySelector('.previous-btn');
+
 nextBtn.addEventListener('click', function (e) {
-  console.log([...UIimage]);
+  let nextIndex;
+  let imgArray = [...UIimage];
+  let testimonyArr = [...UItestimony];
+  console.log(testimonyArr);
+  testimonyArr.forEach((item, index) => {
+    // checked the index of the item with the current class .
+
+    if (item.classList.contains('current')) {
+      nextIndex = index + 1;
+    }
+    // removed the class from the item
+    item.classList.remove('current');
+  });
+  if (nextIndex > testimonyArr.length - 1) {
+    nextIndex = 0;
+  }
+  testimonyArr[nextIndex].classList.add('current');
+
+  imgArray.forEach((item, index) => {
+    // checked the index of the item with the current class .
+
+    if (item.classList.contains('current')) {
+      nextIndex = index + 1;
+    }
+    // removed the class from the item
+    item.classList.remove('current');
+  });
+  if (nextIndex > imgArray.length - 1) {
+    nextIndex = 0;
+  }
+  imgArray[nextIndex].classList.add('current');
+
+  // console.log(nextIndex);
+  e.preventDefault();
+});
+
+previousBtn.addEventListener('click', function (e) {
+  let previousIndex;
+  let imgArray = [...UIimage];
+
+  imgArray.forEach((item, index) => {
+    // checked the index of the item with the current class .
+
+    if (item.classList.contains('current')) {
+      previousIndex = index - 1;
+    }
+    // removed the class from the item
+    item.classList.remove('current');
+  });
+  if (previousIndex < 0) {
+    previousIndex = imgArray.length - 1;
+  }
+  imgArray[previousIndex].classList.add('current');
 
   e.preventDefault();
 });
